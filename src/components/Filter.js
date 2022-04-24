5ee318fbc1c7ff29c0f981e7d9238149260633b0
Melih Budak <melihbudak@gmail.com>
2022-04-24 12:59:40 +0300

    Add filter tab active class name property

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,25 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 
 import {setFilter} from '../redux/todos/todosSlice'
 
 function Filter() {
+    const [selected, setSelected] = useState('all')
     const dispatch = useDispatch()
 
     const handleFilter = (filter) => {
+        setSelected(filter)
         dispatch(setFilter(filter))
     }
 
     return (
         <ul className="filters">
             <li>
-                <a href="#/" className="selected" onClick={() => handleFilter('all')}>All</a>
+                <a href="#/" className={(selected === 'all') ? 'selected': null} onClick={() => handleFilter('all')}>All</a>
             </li>
             <li>
-                <a href="#/" onClick={() => handleFilter('active')}>Active</a>
+                <a href="#/" className={(selected === 'active') ? 'selected': null} onClick={() => handleFilter('active')}>Active</a>
             </li>
             <li>
-                <a href="#/" onClick={() => handleFilter('completed')}>Completed</a>
+                <a href="#/" className={(selected === 'completed') ? 'selected': null} onClick={() => handleFilter('completed')}>Completed</a>
             </li>
         </ul>
     )
